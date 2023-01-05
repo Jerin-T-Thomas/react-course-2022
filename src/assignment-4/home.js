4f08c1c4561c88f7d6e0ab48cecbb42b1fd243eb
Jerin-T-Thomas <jerin.thomas@edstem.com>
2023-01-05 19:15:06 +0530

    Assignment-4

--- src/assignment-4/home.js
+++ src/assignment-4/home.js
@@ -5,7 +5,7 @@
 
 numbers = [2, 3, 5, 7, 9]
 numbers.forEach(element => {
-    console.log(Math.pow(element,2));
+    console.log(Math.pow(element, 2));
 });
 
 
@@ -16,7 +16,7 @@ numbers.forEach(element => {
 input = [1, 2]
 output = []
 input.forEach(element => {
-   output.push(element*2);
+    output.push(element * 2);
 });
 console.log(output)
 
@@ -24,41 +24,41 @@ console.log(output)
 //     Sample Input - [1,2, 3]
 //     Sample Output - [3]
 
-input = [1,2,3]
+input = [1, 2, 3]
 output = []
 input.forEach(element => {
-    if(element%3==0){
-    output.push(element);
+    if (element % 3 == 0) {
+        output.push(element);
     }
- });
- console.log(output)
+});
+console.log(output)
 
 // 4. Given an array of numbers print the product of all numbers
 //     Sample Input - [2,3, 4]
 //     Sample Output - 24
 
-input = [2,3,4]
+input = [2, 3, 4]
 var num = 1
-for(i in input){
+for (i in input) {
     num = num * input[i]
 }
 console.log(num)
 
 // 5. Given an array of strings print the length of each string
 
-input = ['samsung','rollercoaster','pam','apple']
-input.forEach(e =>{
-    console.log('length of ' +e +' is ' +e.length)
+input = ['samsung', 'rollercoaster', 'pam', 'apple']
+input.forEach(e => {
+    console.log('length of ' + e + ' is ' + e.length)
 })
 
 // 6. Given an array of string generate an array with their first characters
 //         Sample Input - ["Edstem", "Tech"]
 //         Sample Output - ["E", "T"]
 
-input = ["Edstem","Tech"]
+input = ["Edstem", "Tech"]
 output = []
 input.forEach(e => {
-output.push(e.charAt(0))
+    output.push(e.charAt(0))
 })
 console.log(output)
 
@@ -70,7 +70,7 @@ input = ["assignment", "problem", "media", "upload"]
 output = []
 input.forEach(e => {
     length = e.length
-    if(e.charAt(0) == 'a' || e.charAt(length-1) == 'a'){
+    if (e.charAt(0) == 'a' || e.charAt(length - 1) == 'a') {
         output.push(e)
     }
 })
@@ -83,11 +83,11 @@ console.log(output)
 input = ["edstem", "tech"]
 var output = ''
 input.forEach(e => {
-    if(e == input[0]){
-    output = e
+    if (e == input[0]) {
+        output = e
     }
-    else{
-    output = output +'_' +e
-    }        
+    else {
+        output = output + '_' + e
+    }
 })
 console.log(output)
